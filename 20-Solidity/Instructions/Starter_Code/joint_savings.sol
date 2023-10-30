ca0f38a8b76a1046db2ded884e4541a3c9fab3a8
sophiepearldgn <84180732+sophiepearldgn@users.noreply.github.com>
2023-10-30 16:48:08 -0400

    screenshots

--- 20-Solidity/Instructions/Starter_Code/joint_savings.sol
+++ 20-Solidity/Instructions/Starter_Code/joint_savings.sol
@@ -1,25 +1,4 @@
-
-
-/*
-Joint Savings Account
----------------------
-
-To automate the creation of joint savings accounts, you will create a solidity smart contract that accepts two user addresses that are then able to control a joint savings account. Your smart contract will use ether management functions to implement various requirements from the financial institution to provide the features of the joint savings account.
-
-The Starting file provided for this challenge contains a `pragma` for solidity version `5.0.0`.
-You will do the following:
-
-1. Create and work within a local blockchain development environment using the JavaScript VM provided by the Remix IDE.
-
-2. Script and deploy a **JointSavings** smart contract.
-
-3. Interact with your deployed smart contract to transfer and withdraw funds.
-
-*/
-
 // SPDX-License-Identifier: UNLICENSED
-
-
 pragma solidity ^0.8.0;
 
 // Define a new contract named `JointSavings`
@@ -42,17 +21,32 @@ contract JointSavings {
         contractBalance += msg.value;
     }
 
+    // Fallback function to receive Ether
+    receive() external payable {
+        // This function allows the contract to store Ether sent from outside the deposit function.
+        contractBalance += msg.value;
+    }
+}
+
+// Define a new contract named `MyContract`
+contract MyContract {
+    address public accountOne;
+    address public accountTwo;
+    address public lastToWithdraw;
+    uint public lastWithdrawAmount;
+    uint public contractBalance;
+
     function withdraw(uint amount, address payable recipient) public {
-        require(recipient == accountOne || recipient == accountTwo, "You don't own this account!");
-        require(contractBalance >= amount, "Insufficient funds!");
+       require(recipient == accountOne || recipient == accountTwo, "You don't own this account!");
+       require(contractBalance >= amount, "Insufficient funds!");
 
-        if (lastToWithdraw != recipient) {
-            lastToWithdraw = recipient;
-        }
+       if (lastToWithdraw != recipient) {
+          lastToWithdraw = recipient;
+       }
 
-        recipient.transfer(amount);
-        lastWithdrawAmount = amount;
-        contractBalance = address(this).balance;
+       recipient.transfer(amount);
+       lastWithdrawAmount = amount;
+       contractBalance = address(this).balance;
     }
 
     function setAccounts(address payable account1, address payable account2) public {
